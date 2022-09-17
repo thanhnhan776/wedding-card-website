91ae532dc333062053c0bb75375523a1872b27d9
nhantt <thanhnhan.toantin@gmail.com>
2022-09-17 18:02:17 +0800

    [feat] Add banner

--- src/app/shared/material/material.module.ts
+++ src/app/shared/material/material.module.ts
@@ -3,10 +3,16 @@ import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [],
   imports: [CommonModule],
-  exports: [MatToolbarModule, MatInputModule, MatFormFieldModule],
+  exports: [
+    MatToolbarModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+  ],
 })
 export class MaterialModule {}
