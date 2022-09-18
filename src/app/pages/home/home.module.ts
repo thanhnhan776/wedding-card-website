f294bddd97ac02c6de8d38ec591a37de352379c5
nhantt <thanhnhan.toantin@gmail.com>
2022-09-18 21:15:08 +0800

    [feat] Add feature products on home page

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -5,9 +5,10 @@ import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home/home.component';
 import { BannerComponent } from './banner/banner.component';
 import { MaterialModule } from 'src/app/shared/material/material.module';
+import { CardsModule } from '../cards/cards.module';
 
 @NgModule({
   declarations: [HomeComponent, BannerComponent],
-  imports: [CommonModule, HomeRoutingModule, MaterialModule],
+  imports: [CommonModule, HomeRoutingModule, MaterialModule, CardsModule],
 })
 export class HomeModule {}
--- src/app/pages/home/home/home.component.ts
+++ src/app/pages/home/home/home.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/data/product/product';
+import { ProductService } from 'src/app/data/product/product.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  featureProduts: Product[] = [];
+  constructor(private productService: ProductService) {}
 
   ngOnInit(): void {
+    this.productService
+      .getFeatureProducts()
+      .subscribe((products: Product[]) => {
+        this.featureProduts = products;
+      });
   }
-
 }
